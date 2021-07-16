c4007fc0b7dbcff98271168362bb92979c68708e
James Green <jameswalkerdine@gmail.com>
2021-07-16 09:36:03 +0100

    adding publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,3 +71,21 @@ tasks.withType<JavaCompile> {
 		dependsOn("publish")
 	}
 }
+
+publishing {
+	repositories {
+		maven {
+			name = "GitHubPackages"
+			url = uri("https://maven.pkg.github.com/simplybusiness/kafka-topic-config")
+			credentials {
+				username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+				password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+			}
+		}
+	}
+	publications {
+		register<MavenPublication>("gpr") {
+			from(components["java"])
+		}
+	}
+}
