bdec3c130e8591218729a484512be98765caa50a
Ivelina Yordanova <ivelina.yordanova@simplybusiness.co.uk>
2022-04-04 16:18:07 +0100

    updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,13 +16,13 @@ tasks.withType<Test> {
 }
 
 plugins {
-	id("org.springframework.boot") version "2.4.4"
+	id("org.springframework.boot") version "2.6.6"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.5.0"
-	kotlin("plugin.spring") version "1.5.0"
+	kotlin("jvm") version "1.6.0"
+	kotlin("plugin.spring") version "1.6.0"
 	id("maven-publish")
 	id("java-library")
-	id("net.researchgate.release") version "2.6.0"
+	id("net.researchgate.release") version "2.8.1"
 	id("com.adarshr.test-logger") version "2.1.1"
 	id("signing")
 	id("java")
@@ -42,16 +42,16 @@ dependencies {
 	testImplementation("org.junit.jupiter","junit-jupiter-engine","5.6.2")
 	testCompileOnly("org.junit.jupiter", "junit-jupiter-params", "5.6.2")
 	implementation("org.apache.kafka:kafka-clients:2.4.0")
-	implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.12.3")
-	implementation("com.fasterxml.jackson.core:jackson-databind:2.12.3")
-	implementation("com.fasterxml.jackson.core:jackson-core:2.12.3")
-	implementation("com.fasterxml.jackson.core:jackson-annotations:2.12.3")
+	implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.2")
+	implementation("com.fasterxml.jackson.core:jackson-databind:2.13.2.2")
+	implementation("com.fasterxml.jackson.core:jackson-core:2.13.2")
+	implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.2")
 
 
-	implementation("org.apache.logging.log4j:log4j-core:2.17.0")
-	implementation("org.apache.logging.log4j:log4j-api:2.17.0")
-	implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.17.0")
-	implementation("commons-io:commons-io:2.8.0")
+	implementation("org.apache.logging.log4j:log4j-core:2.17.1")
+	implementation("org.apache.logging.log4j:log4j-api:2.17.1")
+	implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.17.1")
+	implementation("commons-io:commons-io:2.11.0")
 	testImplementation("com.salesforce.kafka.test:kafka-junit5:3.2.2")
 	testImplementation("org.apache.kafka:kafka_2.12:2.4.0")
 //    implementation("net.researchgate:gradle-release:2.6.0")
